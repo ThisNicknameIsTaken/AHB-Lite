48ef7c523142ec39084e866d4bc2518534b66e78
ThisNicknameIsTaken <32493975+ThisNicknameIsTaken@users.noreply.github.com>
2021-02-10 12:34:37 +0200

    Add files via upload

--- AHB_Lite_decoder.v
+++ AHB_Lite_decoder.v
@@ -2,21 +2,27 @@
 
 module AHB_Lite_decoder(HADDR, HSEL0, HSEL1, HSEL2, HSEL3);
 
+//parameter addr_size = 4;  
+
+
+
 input [`BUS_WIDTH-1:0] HADDR;
 output reg HSEL0, HSEL1, HSEL2, HSEL3;
 
+//wire addr[addr_size-1:0] = HADDR[`BUS_WIDTH-1:`BUS_WIDTH-addr_size];
 
+wire [3:0]  addr= HADDR[31:28];
 
 always @(*) begin
-    casex(HADDR)
-    32'h_0?_??_??_??: begin
+    casex(addr)
+    4'h_0: begin
                       HSEL0 <= 1'b1;
                       HSEL1 <= 1'b0;
                       HSEL2 <= 1'b0;
                       HSEL3 <= 1'b0;
                       end
 
-    32'h_1?_??_??_??: begin
+    4'h_1: begin
                       HSEL0 <= 1'b0;
                       HSEL1 <= 1'b1;
                       HSEL2 <= 1'b0;
@@ -24,7 +30,7 @@ always @(*) begin
                       end
 
     
-    32'h_2?_??_??_??: begin
+    4'h_2: begin
                       HSEL0 <= 1'b0;
                       HSEL1 <= 1'b0;
                       HSEL2 <= 1'b1;
@@ -32,7 +38,7 @@ always @(*) begin
                       end
     
     
-    32'h_3?_??_??_??: begin
+    4'h_3: begin
                       HSEL0 <= 1'b0;
                       HSEL1 <= 1'b0;
                       HSEL2 <= 1'b0;
